4d200f76ea18844714f815d8edcebe86191c5517
Antonio T <h.teran@utp.edu.co>
2023-01-16 00:17:35 -0500

    Orgs added

---
+++ src/app/components/users/profile/organizations/organizations.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { UsersService } from '../../users.service';
+
+@Component({
+  selector: 'app-organizations',
+  templateUrl: './organizations.component.html',
+  styleUrls: ['./organizations.component.scss']
+})
+export class OrganizationsComponent implements OnInit {
+  @Input() login: string = '';
+  loading: boolean = false;
+
+  organizations: any[] = [];
+
+  page: number = 1;
+  per_page: number = 10;
+
+  constructor(private _userService: UsersService) { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    for (const propName in changes) {
+      const chng = changes[propName];
+
+      if (propName == 'login') {
+        this.login = chng.currentValue;
+      }
+    }
+  }
+
+  ngOnInit(): void {
+    this.listOrgs();
+  }
+
+  listOrgs() {
+    this.loading = true;
+    this._userService.listOrgs(this.login, this.per_page, this.page).subscribe((response: any) => {
+      console.log(response);
+      this.organizations = response;
+      this.loading = false;
+    }, error => {
+      this.loading = false;
+    });
+  }
+
+}
